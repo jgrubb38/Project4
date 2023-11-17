b2f1df7f8a3abf02900bd1697a0fb1dd07245c7c
Jennifer Grubb <37131205+jgrubb38@users.noreply.github.com>
2023-11-16 19:46:34 -0700

    Update Gold_Price_DB.sql

--- Gold_Price_DB/Gold_Price_DB.sql
+++ Gold_Price_DB/Gold_Price_DB.sql
@@ -1,13 +1,13 @@
 -- Create a table schema for our table
 CREATE TABLE gld_price_data(
-    Date VARCHAR(10),
+	Date VARCHAR(10),
 	SPX DECIMAL(6, 2),
 	GLD DECIMAL(5, 2),
 	USO DECIMAL(5, 2),
 	SLV DECIMAL(4, 2),
 	EUR2USD DECIMAL(3, 2)
 );
--- Import CSV file into the corresponding SQL table.
+-- Import the CSV file into the corresponding SQL table.
 
 -- Preview data
 SELECT * FROM gld_price_data;
@@ -19,3 +19,5 @@ SELECT * FROM gld_price_data WHERE GLD IS NULL;
 SELECT * FROM gld_price_data WHERE USO IS NULL;
 SELECT * FROM gld_price_data WHERE SLV IS NULL;
 SELECT * FROM gld_price_data WHERE EUR2USD IS NULL;
+
+-- No null values in dataset
